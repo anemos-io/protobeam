57b6f635c80edc69162dff7f90d4a37404f4bac3
Wout Scheepers <Wout.Scheepers@vente-exclusive.com>
2019-02-13 09:41:26 +0100

    Implemented FlattenFieldConvert for GenericRecord

---
+++ convert/src/main/java/io/anemos/protobeam/convert/nodes/genericrecord/FlattenFieldConvert.java
@@ -0,0 +1,30 @@
+package io.anemos.protobeam.convert.nodes.genericrecord;
+
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.DynamicMessage;
+import com.google.protobuf.Message;
+import io.anemos.protobeam.convert.nodes.AbstractMessageConvert;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+
+import java.util.Map;
+
+public class FlattenFieldConvert extends AbstractGenericRecordConvert<Object> {
+
+    private AbstractMessageConvert convert;
+
+    FlattenFieldConvert(Descriptors.FieldDescriptor descriptor, AbstractMessageConvert convert) {
+        super(descriptor);
+        this.convert = convert;
+    }
+
+    @Override
+    public void toProto(GenericRecord row, Message.Builder builder) {
+        DynamicMessage.Builder dynamicMessageBuilder = DynamicMessage.newBuilder(fieldDescriptor.getMessageType());
+        fieldDescriptor.getMessageType().getFields().forEach(subFieldDescriptor -> {
+            Object value = row.get(subFieldDescriptor.getName());
+            dynamicMessageBuilder.setField(subFieldDescriptor, value);
+        });
+        builder.setField(fieldDescriptor, dynamicMessageBuilder.build());
+    }
+}
